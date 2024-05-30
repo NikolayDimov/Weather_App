047300f430a98a99714d722cb9f296cb29ab16e6
Nikolay Dimov <114500017+NikolayDimov@users.noreply.github.com>
2024-05-30 22:21:47 +0300

    fetch weather data

--- App.js
+++ App.js
@@ -3,8 +3,47 @@ import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { Home } from './pages/Home/Home';
 import { ImageBackground } from 'react-native';
 import backgroundImg from './assets/background.png';
+import { useEffect, useState } from 'react';
+import { requestForegroundPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MeteoApi } from './api/meteo';
+
 
 export default function App() {
+  const [coordinates, setCoordinates] = useState();
+  const [weather, setWeather] = useState();
+
+  useEffect(() => {
+    getUserCoordinates();
+  }, []);
+
+  useEffect(() => {
+    if (coordinates) {
+      fetchWeatherByCoords(coordinates);
+    }
+  }, [coordinates]);
+
+  async function fetchWeatherByCoords(coords) {
+    const weatherResponse = await MeteoApi.fetchWeatherByCoords(coords);
+    setWeather(weatherResponse);
+
+  }
+
+  async function getUserCoordinates() {
+    const { status } = await requestForegroundPermissionsAsync();
+    if (status === 'granted') {
+      const location = await getCurrentPositionAsync();
+      setCoordinates({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude
+      });
+    } else {
+      setCoordinates({ lat: '48.85', lng: '2.35' });
+    }
+  };
+
+  console.log('coordinates', coordinates);
+  console.log('weather', weather);
+
   return (
     <ImageBackground imageStyle={s.img} style={s.img_background} source={backgroundImg}>
       <SafeAreaProvider>
--- endpoint.js
+++ endpoint.js
@@ -1,1 +1,3 @@
 https://api.open-meteo.com/v1/forcast
+
+43, 73415099607787, 7, 421467975100671;
